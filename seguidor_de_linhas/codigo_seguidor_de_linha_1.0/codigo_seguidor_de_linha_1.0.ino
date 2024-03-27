4de9c5aca280aaa93a8482da99f1274ca65586a1
Aluno IFAL <aluno.ifal@LABS.RIOLARGO>
2024-03-27 12:06:40 -0300

    new

--- seguidor_de_linhas/codigo_seguidor_de_linha_1.0/codigo_seguidor_de_linha_1.0.ino
+++ seguidor_de_linhas/codigo_seguidor_de_linha_1.0/codigo_seguidor_de_linha_1.0.ino
@@ -10,9 +10,7 @@
 #include <ESPmDNS.h>
 #include <Update.h>
 
-const char* host = "esp32";
-const char* ssid = "IFAL - Rio Largo";
-const char* password = "ifalriolargo";
+
 
 int motorA1 = 27;  //M1
 int motorA2 = 26;  //M1
@@ -25,9 +23,8 @@ int sensor2 = 35;
 
 int valorSensor1;
 int valorSensor2;
-int velocidade = 180;
+int velocidade = 130;
 
-unsigned long ultimoTempo = 0;
 
 //int luminosidade = 30000;
 
@@ -42,97 +39,6 @@ int canalPWM2 = 1;
 int resolution2 = 8;
 
 
-
-WebServer server(80);
-
-/*
- * Login page
- */
-const char* loginIndex =
-  "<form name='loginForm'>"
-  "<table width='20%' bgcolor='A09F9F' align='center'>"
-  "<tr>"
-  "<td colspan=2>"
-  "<center><font size=4><b>ESP32 Login Page</b></font></center>"
-  "<br>"
-  "</td>"
-  "<br>"
-  "<br>"
-  "</tr>"
-  "<td>Username:</td>"
-  "<td><input type='text' size=25 name='userid'><br></td>"
-  "</tr>"
-  "<br>"
-  "<br>"
-  "<tr>"
-  "<td>Password:</td>"
-  "<td><input type='Password' size=25 name='pwd'><br></td>"
-  "<br>"
-  "<br>"
-  "</tr>"
-  "<tr>"
-  "<td><input type='submit' onclick='check(this.form)' value='Login'></td>"
-  "</tr>"
-  "</table>"
-  "</form>"
-  "<script>"
-  "function check(form)"
-  "{"
-  "if(form.userid.value=='admin' && form.pwd.value=='admin')"
-  "{"
-  "window.open('/serverIndex')"
-  "}"
-  "else"
-  "{"
-  " alert('Error Password or Username')/*displays error message*/"
-  "}"
-  "}"
-  "</script>";
-
-/*
- * Server Index Page
- */
-
-const char* serverIndex =
-  "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
-  "<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
-  "<input type='file' name='update'>"
-  "<input type='submit' value='Update'>"
-  "</form>"
-  "<div id='prg'>progress: 0%</div>"
-  "<script>"
-  "$('form').submit(function(e){"
-  "e.preventDefault();"
-  "var form = $('#upload_form')[0];"
-  "var data = new FormData(form);"
-  " $.ajax({"
-  "url: '/update',"
-  "type: 'POST',"
-  "data: data,"
-  "contentType: false,"
-  "processData:false,"
-  "xhr: function() {"
-  "var xhr = new window.XMLHttpRequest();"
-  "xhr.upload.addEventListener('progress', function(evt) {"
-  "if (evt.lengthComputable) {"
-  "var per = evt.loaded / evt.total;"
-  "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
-  "}"
-  "}, false);"
-  "return xhr;"
-  "},"
-  "success:function(d, s) {"
-  "console.log('success!')"
-  "},"
-  "error: function (a, b, c) {"
-  "}"
-  "});"
-  "});"
-  "</script>";
-
-/*
- * setup function
- */
 void setup() {
 
   pinMode(motorA1, OUTPUT);
@@ -151,77 +57,8 @@ void setup() {
   ledcWrite(canalPWM2, 0);
   Serial.begin(115200);
 
-  // Connect to WiFi network
-  WiFi.begin(ssid, password);
-  Serial.println("");
-
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  /*use mdns for host name resolution*/
-  if (!MDNS.begin(host)) {  //http://esp32.local
-    Serial.println("Error setting up MDNS responder!");
-    while (1) {
-      delay(1000);
-    }
-  }
-  Serial.println("mDNS responder started");
-  /*return index page which is stored in serverIndex */
-  server.on("/", HTTP_GET, []() {
-    server.sendHeader("Connection", "close");
-    server.send(200, "text/html", loginIndex);
-  });
-  server.on("/serverIndex", HTTP_GET, []() {
-    server.sendHeader("Connection", "close");
-    server.send(200, "text/html", serverIndex);
-  });
-  /*handling uploading firmware file */
-  server.on(
-    "/update", HTTP_POST, []() {
-      server.sendHeader("Connection", "close");
-      server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
-      ESP.restart();
-    },
-    []() {
-      HTTPUpload& upload = server.upload();
-      if (upload.status == UPLOAD_FILE_START) {
-        Serial.printf("Update: %s\n", upload.filename.c_str());
-        if (!Update.begin(UPDATE_SIZE_UNKNOWN)) {  //start with max available size
-          Update.printError(Serial);
-        }
-      } else if (upload.status == UPLOAD_FILE_WRITE) {
-        /* flashing firmware to ESP*/
-        if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
-          Update.printError(Serial);
-        }
-      } else if (upload.status == UPLOAD_FILE_END) {
-        if (Update.end(true)) {  //true to set the size to the current progress
-          Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
-        } else {
-          Update.printError(Serial);
-        }
-      }
-    });
-  server.begin();
 }
-
 void loop() {
-  // server.handleClient();
-  // delay(1);
-
-
-
-
-
-
   //delay(2000);
   valorSensor1 = !digitalRead(sensor1); // 1 ligado, 0 desligado.
   valorSensor2 = !digitalRead(sensor2); // 1 ligado, 0 desligado.
@@ -249,9 +86,9 @@ void loop() {
     ledcDetachPin(motorA2);
   }
   if (valorSensor1 == 1 && valorSensor2 == 0) {
-    ledcAttachPin(motorA1, canalPWM);
+    ledcAttachPin(motorA2, canalPWM);
     ledcWrite(canalPWM, velocidade);
-    digitalWrite(motorA2, LOW);
+    digitalWrite(motorA1, LOW);
 
     ledcDetachPin(motorB1);
     ledcDetachPin(motorB2);
